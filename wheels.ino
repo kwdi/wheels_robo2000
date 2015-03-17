29f3130957703e15fc414abb18b5ac35da56fca1
kostasdi <kostis-x@hotmail.com>
2015-03-17 21:27:20 +0200

    basic movement

--- wheels.ino
+++ wheels.ino
@@ -1,9 +1,10 @@
-
+#include <Wire.h>
 #include <Encoder.h>
 
 Encoder E1(2, 3); //UpRight
 Encoder E2(18, 19); //UPLeft
 
+const int Speed = 180;
 
 
 int DirectionPin1 = 50; //direction of rotation of the right wheel (forward HIGH, LOW back) 
@@ -18,10 +19,13 @@ int enablePin2 = 49;
 
 void setup()
 {
+  Wire.begin(2);        // join i2c bus
+  Wire.onReceive(receiveEvent); // register event
+
+  Serial.begin(9600);
   
   pinMode(DirectionPin1, OUTPUT); //direction of rotation of the right wheel 
   pinMode(DirectionPin2, OUTPUT); //direction of rotation of the left wheel 
-  pinMode(lederror, OUTPUT);
   pinMode(ShutPin1, OUTPUT);
   pinMode(PwmPin1, OUTPUT);
   pinMode(enablePin1, OUTPUT);
@@ -37,6 +41,52 @@ void setup()
   //
 }
 
+void go(){
+  digitalWrite(DirectionPin1, HIGH);
+  analogWrite(PwmPin1, Speed);                              
+  digitalWrite(DirectionPin2, HIGH); 
+  analogWrite(PwmPin2, Speed); 
+}
+
+void backwards(){
+  digitalWrite(DirectionPin1, LOW);
+  analogWrite(PwmPin1, Speed);                              
+  digitalWrite(DirectionPin2, LOW); 
+  analogWrite(PwmPin2, Speed);
+}
+
+void turnleft(int t){
+  digitalWrite(DirectionPin1, LOW);
+  analogWrite(PwmPin1, Speed);                              
+  digitalWrite(DirectionPin2, HIGH); 
+  analogWrite(PwmPin2, Speed);
+  delay(t);
+}
+
+void turnright(int t){
+  digitalWrite(DirectionPin1, HIGH);
+  analogWrite(PwmPin1, Speed);                              
+  digitalWrite(DirectionPin2, LOW); 
+  analogWrite(PwmPin2, Speed);
+  delay(t);
+}  
+
+void stopmove(){
+  analogWrite(PwmPin1, 0);                              
+  analogWrite(PwmPin2, 0);
+}  
+
+void receiveEvent(int howMany)
+{
+  while(1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
 void loop()
 {
+  delay(100);
 }
